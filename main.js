d3616859797ad6d7e32b433a82ab9a8569ac27c8
Iuri <iurimedina@gmail.com>
2021-11-07 21:19:52 -0300

    Changes at the start button.

--- main.js
+++ main.js
@@ -1,51 +1,52 @@
-const hour = document.querySelector('#hour')
-const minute = document.querySelector('#minute')
-const second = document.querySelector('#second')
+const hour = document.querySelector('#hour');
+const minute = document.querySelector('#minute');
+const second = document.querySelector('#second');
 
-let num1, num2, num3 = 0
+let num1, num2, num3 = 0;
 
-const startBtn = document.querySelector('#start')
-const stopBtn = document.querySelector('#stop')
-const resetBtn = document.querySelector('#reset')
+const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
+const resetBtn = document.querySelector('#reset');
 
 function checkWatch() {
-    if(second.innerHTML || minute.innerHTML || hour.innerHTML !== '00') {
-        startBtn.disabled = true
+    if (second.innerHTML || minute.innerHTML || hour.innerHTML !== '00') {
+        startBtn.disabled = true;
     }
 }
 
-let interval
+let interval;
 
-const start = startBtn.addEventListener("click",(e) => {
-        interval = setInterval(() => {
-        num1 = ++second.innerHTML
-        second.innerHTML = num1
-        if(num1 > 60) {
-            num2 = ++minute.innerHTML
-            minute.innerHTML = num2
-            second.innerHTML = 0
-    
-            if(num2 > 60) {
-                num3 = ++hour.innerHTML
-                hour.innerHTML = num3
-                minute.innerHTML = 0
+const start = startBtn.addEventListener("click", (e) => {
+    interval = setInterval(() => {
+        num1 = ++second.innerHTML;
+        second.innerHTML = num1;
+        if (num1 > 60) {
+            num2 = ++minute.innerHTML;
+            minute.innerHTML = num2;
+            second.innerHTML = 0;
+
+            if (num2 > 60) {
+                num3 = ++hour.innerHTML;
+                hour.innerHTML = num3;
+                minute.innerHTML = 0;
             }
         }
-    }, 1000), checkWatch()}
+    }, 1000), checkWatch();
+}
 )
 
 const stop = stopBtn.addEventListener("click", (e) => {
-    clearInterval(interval)
-    startBtn.disabled = false
-    
+    clearInterval(interval);
+    startBtn.disabled = false;
+
 })
 
 const reset = resetBtn.addEventListener("click", (e) => {
-    clearInterval(interval)
-    second.innerHTML = '00'
-    minute.innerHTML = '00'
-    hour.innerHTML = '00'
-    startBtn.disabled = false
+    clearInterval(interval);
+    second.innerHTML = '00';
+    minute.innerHTML = '00';
+    hour.innerHTML = '00';
+    startBtn.disabled = false;
 })
 
 
