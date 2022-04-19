dbaa83ea43aa4821941bbacb7c6b5b99a6c5417b
Iuri <iurimedina@gmail.com>
2022-04-18 22:28:26 -0300

    Mudanças na lógica e formatação do relógio

--- main.js
+++ main.js
@@ -1,58 +1,59 @@
-const hour = document.querySelector('#hour');
-const minute = document.querySelector('#minute');
-const second = document.querySelector('#second');
-
-let num1 = 0, num2 = 0, num3 = 0;
+const timer = document.getElementById('timer');
+let hour = 0, minute = 0, second = 0;
+let interval;
 
 const startBtn = document.querySelector('#start');
 const stopBtn = document.querySelector('#stop');
 const resetBtn = document.querySelector('#reset');
-
-function checkWatch() {
-    if (second.innerHTML || minute.innerHTML || hour.innerHTML !== '00') {
-        startBtn.disabled = true;
-    }
-}
-
-let interval;
+startBtn.disabled = false;
 
 const start = startBtn.addEventListener("click", (e) => {
     interval = setInterval(() => {
-        num1 = ++second.innerHTML;
-        second.innerHTML = num1;
-        if (num1 > 60) {
-            num2 = ++minute.innerHTML;
-            minute.innerHTML = num2;
-            second.innerHTML = 0;
+        second = parseInt(second);
+        minute = parseInt(minute);
+        hour = parseInt(hour);
+        
+        second = ++second;
+        if (second > 60) {
+            minute = ++minute;
+            second = 0;
+        };
 
-            if (num2 > 60) {
-                num3 = ++hour.innerHTML;
-                hour.innerHTML = num3;
-                minute.innerHTML = 0;
-            }
-        }
-    }, 1000), checkWatch();
+        if (minute > 60) {
+            hour = ++hour;
+            minute = 0;
+            second = 0;
+        };
+
+        if (second < 10 || second == 0) {
+            second = "0" + second;
+        };
+
+        if (minute < 10 || minute == 0) {
+            minute = "0" + minute;
+        };
+
+        if (hour < 10 || hour == 0) {
+            hour = "0" + hour;
+        };
+
+        timer.innerHTML = hour + ":" + minute + ":" + second;
+
+    }, 10), startBtn.disabled = true;
+
 }
 )
 
 const stop = stopBtn.addEventListener("click", (e) => {
     clearInterval(interval);
     startBtn.disabled = false;
-
 })
 
 const reset = resetBtn.addEventListener("click", (e) => {
     clearInterval(interval);
-    second.innerHTML = '00';
-    minute.innerHTML = '00';
-    hour.innerHTML = '00';
+    timer.innerHTML = "00:00:00";
+    second = 0;
+    minute = 0;
+    hour = 0;
     startBtn.disabled = false;
 })
-
-
-
-
-
-
-
-
